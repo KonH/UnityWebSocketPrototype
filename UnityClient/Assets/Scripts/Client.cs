eeab4922971188f24ae3dd7e1fbc9e73d22de7a4
konh <konh@yandex.ru>
2020-04-23 20:29:57 +0700

    Implement Unity web socket client

---
+++ UnityClient/Assets/Scripts/Client.cs
@@ -0,0 +1,41 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Client : MonoBehaviour {
+	[SerializeField]
+	TMP_InputField _messageInput;
+
+	[SerializeField]
+	Button _sendButton;
+
+	[SerializeField]
+	TMP_Text _messagesText;
+
+	SignalRLib _ws;
+
+	void Awake() {
+		_sendButton.onClick.AddListener(OnSend);
+	}
+
+	void Start() {
+		_ws = new SignalRLib();
+		_ws.Init("http://localhost:5000/chat/", "Send");
+		_ws.ConnectionStarted += (sender, args) => {
+			AddText(args.Message);
+		};
+		_ws.MessageReceived += (sender, args) => {
+			AddText(args.Message);
+		};
+	}
+
+	void OnSend() {
+		var message = _messageInput.text;
+		_messageInput.text = "";
+		_ws.SendMessage("Send", message);
+	}
+
+	void AddText(string text) {
+		_messagesText.text += $"{text}\n";
+	}
+}
